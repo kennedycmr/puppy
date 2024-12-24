3ef1af3e4c44a7dd07742cfc16632c7ab28e46ae
Cameron Kennedy <kennedycmr@gmail.com>
2024-12-24 14:38:01 -0500

    add BigBark(s) functions

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/kennedycmr/puppy
 
 go 1.22.9
+
+require github.com/kennedycmr/dog v0.0.0-20241224193327-1d06d0f61b43 // indirect
--- puppy.go
+++ puppy.go
@@ -1,9 +1,21 @@
 package puppy
 
+import (
+	"github.com/kennedycmr/dog"
+)
+
 func Bark() string {
 	return "Woof!"
 }
 
 func Barks() string {
 	return "Woof! Woof! Woof!"
+}
+
+func BigBark() {
+	return dog.WhenGrownUp(Bark())
+}
+
+func BigBarks() {
+	return dog.WhenGrownUp(Barks())
 }
